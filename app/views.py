d8d31302e49e0350aa75b09fb5240d0d5f4abf09
saeed <saeedmr881@gmail.com>
2021-05-04 13:32:21 +0430

    scrapy

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse
+import app.gold_scrapy as gold_scrapy
 
 # Create your views here.
 
 def home(request):
     t = loader.get_template('home.html')
-    return HttpResponse(t.render({}, request))
+    gold_price ,gold_change ,dollar_price,dollar_change,stock_price,stock_change  =gold_scrapy.price()
+    return HttpResponse(t.render({"gold_price":gold_price,"gold_change":gold_change,
+                                  "dollar_price":dollar_price,"dollar_change":dollar_change,
+                                  "stock_price":stock_price,"stock_change":stock_change,}, request))
